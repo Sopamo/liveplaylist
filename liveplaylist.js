10730c910d867eec6564594c52b3e8bdbbd8236a
Paul Mohr <github@sopamo.de>
2015-04-19 11:55:47 +0200

    Some inital stuff

--- liveplaylist.js
+++ liveplaylist.js
@@ -1,23 +1,67 @@
+Videos = new Mongo.Collection("videos");
+Channels = new Mongo.Collection("channels");
+
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
+    // This code only runs on the client
+    Template.body.helpers({
+        videos: function () {
+            return Videos.find({});
+        }
+    });
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
+    Template.body.events({
+        "submit .add-video": function (event) {
+            // This function is called when the new video form is submitted
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
+            var url = event.target.text.value;
+
+            var params = getQueryParams(url);
+            console.log(params);
+            $.ajax({url: "https://gdata.youtube.com/feeds/api/videos/" + params.v + "?v=2&alt=json"}).done(function (data) {
+                Videos.insert({
+                    title: data.entry.title.$t,
+                    ytid: params.v
+                });
+            });
+
+            // Clear form
+            event.target.text.value = "";
+
+            // Prevent default form submit
+            return false;
+        }
+    });
+
+    Channels.findOne({
+        "slug": "sopamo" // TODO: Use the current channel here
+    }).observeChanges({
+        added: function () {
+        },
+        changed: function (id, fields) {
+            console.log(fields);
+        },
+        removed: function () {
+        }
+    });
+
+    function getQueryParams(qs) {
+        qs = qs.split("?")[1];
+        qs = qs.split("+").join(" ");
+
+        var params = {}, tokens,
+                re = /[?&]?([^=]+)=([^&]*)/g;
+
+        while (tokens = re.exec(qs)) {
+            params[decodeURIComponent(tokens[1])]
+                    = decodeURIComponent(tokens[2]);
+        }
+
+        return params;
     }
-  });
 }
 
 if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
+    Meteor.startup(function () {
+        // code to run on server at startup
+    });
 }
