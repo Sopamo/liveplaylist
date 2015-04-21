2b3edb77fcb2d5c853353ba380bd4be18fcbeed5
Paul Mohr <github@sopamo.de>
2015-04-21 13:45:32 +0200

    Implemented synced start / pause / seek

--- liveplaylist.js
+++ liveplaylist.js
@@ -2,6 +2,7 @@ Videos = new Mongo.Collection("videos");
 Channels = new Mongo.Collection("channels");
 var player = null;
 var channelSlug = "sopamo"; // TODO: Use the current channel here
+var currentVideo = "";
 
 if (Meteor.isClient) {
     onYouTubeIframeAPIReady = function () {
@@ -10,8 +11,9 @@ if (Meteor.isClient) {
                 alert("Whoops, an error occured. Try to reload the page.");
                 return;
             }
-            // New Video Player, the first argument is the id of the div.
-            // Make sure it's a global variable.
+            
+            currentVideo = channel.active;
+            
             player = new YT.Player("player", {
                 height: "400",
                 width: "600",
@@ -23,6 +25,25 @@ if (Meteor.isClient) {
                     onReady: function (event) {
                         // Play video when player ready.
                         event.target.playVideo();
+                    },
+                    onStateChange: function(event) {
+                        switch (event.data) {
+                            case 0: // Ended
+                                // Start next video
+                                //var currentTime = event.target.getCurrentTime();
+                                //Meteor.call("setVideoStatus", channelSlug, event.data, 0);
+                                break;
+                            case 1: // Now playing
+                                // Propagate start
+                                var currentTime = event.target.getCurrentTime();
+                                Meteor.call("setVideoStatus",channelSlug,event.data,currentTime);
+                                break;
+                            case 2: // Paused
+                                // Propagate stop
+                                var currentTime = event.target.getCurrentTime();
+                                Meteor.call("setVideoStatus", channelSlug, event.data, currentTime);
+                                break;
+                        }
                     }
                 }
             });
@@ -57,7 +78,6 @@ if (Meteor.isClient) {
             var url = event.target.text.value;
 
             var params = getQueryParams(url);
-            console.log(params);
             $.ajax({url: "https://gdata.youtube.com/feeds/api/videos/" + params.v + "?v=2&alt=json"}).done(function (data) {
                 Videos.insert({
                     title: data.entry.title.$t,
@@ -77,7 +97,7 @@ if (Meteor.isClient) {
         'click .video-entry': function (event, template) {
             Meteor.call('changeVideo', channelSlug, $(event.target).data("videoid"), function (error, result) {
                 if (error) {
-                    alert("Couln't change video :(");
+                    alert("Couldn't change video :(");
                 }
             });
         }
@@ -89,7 +109,23 @@ if (Meteor.isClient) {
         });
 
         if (player != null) {
-            player.loadVideoById(c.active, 0);
+            if(currentVideo != c.active) {
+                // Play the active video if it is not the one already playing
+                currentVideo = c.active;
+                player.loadVideoById(c.active, 0);
+            } else {
+                console.log(c);
+                // Check which state we have
+                if(c.status == 1) {
+                    // We are now playing the video but it is the same video like before. Go to the given position in the video.
+                    // TODO: Implement lag compensation with currentTimeUpdated
+                    player.seekTo(c.currentTime,true);
+                    player.playVideo();
+                } else if(c.status == 2) {
+                    player.pauseVideo();
+                }
+            }
+            
         }
     });
     
@@ -117,7 +153,13 @@ if (Meteor.isServer) {
             Videos.insert({title: "01. My Dear Frodo- The Hobbit: An Unexpected Journey- Soundtrack", ytid: "_gwLxntIfZY"});
         }
         if (Channels.find().count() === 0) {
-            Channels.insert({slug: "sopamo",active:"IBH4g_ua5es"});
+            Channels.insert({
+                slug: "sopamo",
+                active:"IBH4g_ua5es",
+                currentStatus:1,
+                currentTime:0,
+                currentTimeUpdated: 1429615671
+            });
         }
     });
 
@@ -140,7 +182,26 @@ if (Meteor.isServer) {
                         slug: channelSlug
                     }, {
                         $set: {
-                            active: videoId
+                            active: videoId,
+                            currentTime: 0,
+                            currentTimeUpdated: Math.floor(Date.now() / 1000)
+                        }
+                    });
+
+            return true;
+        },
+        setVideoStatus: function(channelSlug, status, time) {
+            // Check argument types
+            check(channelSlug, String);
+
+            Channels.update(
+                    {
+                        slug: channelSlug
+                    }, {
+                        $set: {
+                            status: status,
+                            currentTime: time,
+                            currentTimeUpdated: Math.floor(Date.now() / 1000)
                         }
                     });
 
